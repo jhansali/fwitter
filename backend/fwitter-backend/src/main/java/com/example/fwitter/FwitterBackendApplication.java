c9bda3d2a4ef61994b29c5b21b6a1c4debd2b91e
jhansali <jhansali@asu.edu>
2024-09-02 17:59:21 -0700

    first commit

---
+++ backend/fwitter-backend/src/main/java/com/example/fwitter/FwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwitterBackendApplication.class, args);
+	}
+
+}
---
+++ backend/fwitter-backend/src/test/java/com/example/fwitter/FwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
