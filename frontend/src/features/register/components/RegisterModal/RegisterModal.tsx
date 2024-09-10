dee91e14e31a01e73869071975638dbde2073cfd
jhansali <jhansali@asu.edu>
2024-09-09 19:34:45 -0700

    Add basic frontend for Register Page

---
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Modal } from '../../../../modal/Modal'
+import './RegisterModal.css';
+
+const RegisterModal:React.FC = () => {
+  return (
+    <div className='register-modal'>
+      <Modal>
+        <h1>Register</h1>
+      </Modal>
+    </div>
+  )
+}
+
+export default RegisterModal
